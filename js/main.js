ffc3f2d11901331a1c9bf0489b828cb596eae611
Levi Jones <leviwill.j@gmail.com>
2025-05-31 22:09:54 -0400

    mobile nav

--- js/main.js
+++ js/main.js
@@ -1,31 +1,60 @@
+(function() {
+  let navOpen = false;
 
-// shorthand lol
-function createElement(element) {
-  return document.createElement(element)
-}
+  // shorthand lol
+  function createElement(element) {
+    return document.createElement(element)
+  }
 
-// nav start
-const nav = document.getElementById('navigation')
-const tabs = [
-  {link: '/', displayText: 'Home'},
-  {link: '/rsvp.html', displayText: 'RSVP'},
-  {link: '/questions.html', displayText: 'Questions'},
-  {link: '/registry.html', displayText: 'Registry'},
-]
+  // nav start
+  const nav = document.getElementById('navigation')
+  const tabs = [
+    {link: '/', displayText: 'Home'},
+    {link: '/rsvp.html', displayText: 'RSVP'},
+    {link: '/questions.html', displayText: 'Questions'},
+    {link: '/registry.html', displayText: 'Registry'},
+  ]
 
-const ul = createElement('ul')
-tabs.forEach((tab) => {
-  // create list item and link
-  const li = createElement('li')
-  const a = createElement('a')
-  // set link's attributes
-  a.setAttribute('href', tab.link)
-  a.textContent = tab.displayText
-  // append ul < li < a
-  li.appendChild(a)
-  ul.appendChild(li)
-})
+  const ul = createElement('ul')
+  const burger = createElement('button');
 
-nav.appendChild(ul)
-// nav end
+  ul.classList.add('hideOnMobile');
+  tabs.forEach((tab) => {
+    const li = createElement('li')
+    const a = createElement('a')
 
+    a.setAttribute('href', tab.link)
+    a.textContent = tab.displayText
+
+    // Set active styling on the currently selected tab
+    const currentPath = window.location.pathname
+    if ((tab.link === '/' && (currentPath === '/' || currentPath === '/index.html')) ||
+        (tab.link !== '/' && currentPath.endsWith(tab.link))) {
+      a.classList.add('active');
+    }
+
+    // append ul < li < a
+    li.appendChild(a)
+    ul.appendChild(li)
+  })
+
+  // Create hamburger menu button
+  burger.textContent = '☰';
+  burger.onclick = e => {
+    e.preventDefault();
+    toggleNav();
+  }
+
+  const toggleNav = () => {
+    if (navOpen) {
+      burger.textContent = '☰';
+    } else {
+      burger.textContent = '✕';
+    }
+    ul.classList.toggle('hideOnMobile');
+    navOpen = !navOpen;
+  }
+
+  nav.appendChild(burger);
+  nav.appendChild(ul)
+})();
